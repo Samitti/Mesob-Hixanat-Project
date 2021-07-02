338f0dc6a5bd06edf1f503d893d2ec6ddcf2aaba
Samitti <samuelteweldebrhan@gmail.com>
2021-07-02 13:47:19 +0300

    initial setup

---
+++ src/Components/Login.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Login = (props) => {
+    return (
+        <Container>
+            <Content>Content</Content>
+        </Container>
+    )
+};
+
+const Container = styled.section`
+
+`
+
+const Content = styled.section`
+
+` 
+
+export default Login;
